c9d7129a1369b54a219fc2c064bb28afb3be400f
metnversi <nguyenthevinh2002.xuantruong@gmail.com>
2024-02-27 19:45:55 +0700

    init

---
+++ a/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace a.ViewModels;
+
+public partial class HomeViewModel : ObservableObject
+{
+    public HomeViewModel()
+    {
+    }
+
+}
---
+++ a/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using a.ViewModels;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace a;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    public HomeViewModel HomeViewModel { get; }
+    
+    public MainWindowViewModel()
+    {
+        HomeViewModel = new HomeViewModel();
+    }
+
+}
