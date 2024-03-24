9a5cf00457d0cf751f38d87ecc00ee3fcecd14d6
metnversi <nguyenthevinh2002.xuantruong@gmail.com>
2024-03-24 23:33:14 +0700

    :)

--- a/Views/MainWindow.xaml.cs
+++ a/Views/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ public partial class MainWindow
     {
         
         InitializeComponent();
+        
         HomeView.DataContext = hvm;
         DataView.DataContext = dvm;
         CommandBindings.Add(new CommandBinding(ApplicationCommands.Close, OnClose));
@@ -22,16 +23,4 @@ public partial class MainWindow
     {
         Close();
     }
-
-    //private void ToggleNav_Checked(object sender, RoutedEventArgs e)
-    //{
-    //    NavTabControl.Width = 200;
-    //    ToggleNav.Content = "\u25C0"; 
-    //}
-
-    //private void ToggleNav_Unchecked(object sender, RoutedEventArgs e)
-    //{
-    //    NavTabControl.Width = 0;
-    //    ToggleNav.Content = "\u25B6"; 
-    //}
 }
